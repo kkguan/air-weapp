2fb39aed07936572d47cee74a43b9eb929e17d27
azou <zouxin@kkguan.com>
2018-06-15 16:08:55 +0800

    feat: add color examples

---
+++ examples/pages/color/index.js
@@ -0,0 +1,17 @@
+Page({
+    data: {
+        primary: '#2d8cf0',
+        lightPrimary: '#5cadff',
+        darkPrimary: '#2b85e4',
+        success: '#19be6b',
+        warning: '#ff9900',
+        error: '#ed3f14',
+        title: '#1c2438',
+        content: '#495060',
+        subColor: '#ed3f14',
+        disabled: '#bbbec4',
+        border: '#dddee1',
+        divider: '#e9eaec',
+        background: '#f8f8f9'
+    }
+})
