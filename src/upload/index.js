ee9fae87e0132ec184ae7ee3b2ecf8722cb3570e
azou <zouxin@kkguan.com>
2018-06-20 19:32:32 +0800

    update

---
+++ src/upload/index.js
@@ -0,0 +1,15 @@
+Component({
+    externalClasses: ['air-class'],
+
+    properties: {
+        // add | camera | custom
+        type: {
+            type: String,
+            value: 'add'
+        },
+        color: {
+            type: String,
+            value: '#8c939d'
+        }
+    }
+})
